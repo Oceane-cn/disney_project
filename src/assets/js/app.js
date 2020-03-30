29e60f781be67221d269072c6264aba097a078ee
Oceane.cn <cn.oceane@gmail.com>
2020-03-30 17:23:50 +0200

    [JS]add quote changes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,14 +1,30 @@
+var citation = document.getElementById("citation");
 var citations = [
-  "”Remember, you’re the one who can fill the world with sunshine”- Snow White",
-  "“My heart has wings and I can fly. I’ll touch every star in the sky. So this is the miracle I’ve been dreaming of.”-Cinderella",
-  "“Love is putting someone else’s needs before your own.”-Frozen",
-  "“If you dream a thing more than once, it’s sure to come true.”- Sleeping Beauty",
-  "“Love doesn’t have to be perfect. It just needs to be true.”- Beauty and the Beast",
-  "“Today’s special moments are tomorrow’s memories”- Aladdin",
-  "“You know that place between sleep and awake, the place where you can still remember dreaming? That’s where I’ll always love you. That’s where I’ll be waiting.”-Peter Pan",
-  "“You don’t really need someone to complete you… You only need someone to accept you completely” – Tangled",
-  "“I don’t understand how a world that makes such wonderful things could be bad”-The Little Mermaid"
+  "”Remember, you’re the one who can fill the world with sunshine” - Snow White",
+  "“If you keep on believing, the dream that you wish will come true.” - Cinderella",
+  "“Love is putting someone else’s needs before your own.” - Frozen",
+  "“If you dream a thing more than once, it’s sure to come true.” - Sleeping Beauty",
+  "“Love doesn’t have to be perfect. It just needs to be true.” - Beauty and the Beast",
+  "“Today’s special moments are tomorrow’s memories” - Aladdin",
+  "“Ohana means family. Family means nobody gets left behind.” - Lilo & Stitch",
+  "“If you can’t say something nice, don’t say nothing at all.” – Bambi",
+  "“Ladies don’t start fights, but they can finish them!” - The Aristocats"
 ];
+var count = 1;
 
-var citation = document.getElementById["citation"];
-console.log(citation);
+function change_citation() {
+  if (count >= citations.length) {
+    count = 0;
+    citation.innerHTML =
+      "<p class='description__citation'>" + citations[count] + "</p>";
+    citation.classList.add("active");
+    count++;
+  } else {
+    citation.innerHTML =
+      "<p class='description__citation'>" + citations[count] + "</p>";
+    count++;
+  }
+  console.log(count);
+}
+
+setInterval(change_citation, 3000);
