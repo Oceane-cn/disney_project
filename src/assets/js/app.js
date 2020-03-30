8a795f86822ecdc3430f80ad9ad90c8dea9ab824
Oceane.cn <cn.oceane@gmail.com>
2020-03-30 16:14:02 +0200

    [JS]add app.js and minor changes to css

---
+++ src/assets/js/app.js
@@ -0,0 +1,14 @@
+var citations = [
+  "”Remember, you’re the one who can fill the world with sunshine”- Snow White",
+  "“My heart has wings and I can fly. I’ll touch every star in the sky. So this is the miracle I’ve been dreaming of.”-Cinderella",
+  "“Love is putting someone else’s needs before your own.”-Frozen",
+  "“If you dream a thing more than once, it’s sure to come true.”- Sleeping Beauty",
+  "“Love doesn’t have to be perfect. It just needs to be true.”- Beauty and the Beast",
+  "“Today’s special moments are tomorrow’s memories”- Aladdin",
+  "“You know that place between sleep and awake, the place where you can still remember dreaming? That’s where I’ll always love you. That’s where I’ll be waiting.”-Peter Pan",
+  "“You don’t really need someone to complete you… You only need someone to accept you completely” – Tangled",
+  "“I don’t understand how a world that makes such wonderful things could be bad”-The Little Mermaid"
+];
+
+var citation = document.getElementById["citation"];
+console.log(citation);
