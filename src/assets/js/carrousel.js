ad222213222ab0787e2480259509dd748f6f562d
Oceane.cn <cn.oceane@gmail.com>
2020-04-03 16:41:41 +0200

    [JS]fix carrousel and showing book information bugs for responsive

--- src/assets/js/carrousel.js
+++ src/assets/js/carrousel.js
@@ -1,3 +1,5 @@
+//Variables pour le carrousel
+
 var desktop = 1025;
 var carrousel = document.getElementById("carrousel");
 var figures = carrousel.querySelectorAll(".books__book");
@@ -6,13 +8,23 @@ var right = document.getElementById("rightSlide");
 var max = figures.length;
 var book_tab = [0, 1, 2];
 
+//Variables pour montrer et cacher les informations des livres
+
+var more = document.getElementsByClassName("book__more");
+var infos = document.getElementsByClassName("book__informations");
+var books_carrousel = document.getElementsByClassName("books__book");
+var close = document.getElementsByClassName("close");
+var active = 0;
+
 WindowSize();
 
 function WindowSize() {
+  console.log(active);
   var larg = document.body.clientWidth;
-  if (larg >= desktop) {
+  if (larg >= desktop && active != 1) {
     slide();
-  } else {
+  } else if (active != 1) {
+    console.log("coucou");
     for (let i = 0; i < max; i++) {
       figures[i].classList.remove("hidden");
       if (figures[i].classList.contains("books__biger")) {
@@ -43,7 +55,6 @@ function booksToShowLeft() {
   for (let i = 0; i < book_tab.length; i++) {
     if (book_tab[i] <= 0) {
       book_tab[i] = 8;
-      console.log(book_tab[i]);
     } else {
       book_tab[i]--;
     }
@@ -67,3 +78,60 @@ left.addEventListener("click", booksToShowLeft);
 right.addEventListener("click", booksToShowRight);
 
 window.onresize = WindowSize;
+
+//Fonctions pour montrer et cacher les informations des livres
+
+function show_more(e) {
+  let book = e.target.closest(".books__book");
+  let id = book.id;
+  active = 1;
+  console.log(active);
+
+  for (let j = 0; j < infos.length; j++) {
+    if (infos[j].classList.contains(id)) {
+      infos[j].classList.add("active");
+    }
+  }
+
+  hide_books();
+  left.style.display = "none";
+  right.style.display = "none";
+}
+
+function close_book(e) {
+  let current_book = e.target.closest(".informations");
+  current_book.classList.remove("active");
+
+  show_books();
+
+  active = 0;
+}
+
+function show_books() {
+  var larg = document.body.clientWidth;
+  if (larg >= desktop) {
+    slide();
+    left.style.display = "inline-block";
+    right.style.display = "inline-block";
+  } else {
+    for (let k = 0; k < books_carrousel.length; k++) {
+      books_carrousel[k].classList.remove("hidden");
+    }
+  }
+}
+
+function hide_books() {
+  for (let k = 0; k < books_carrousel.length; k++) {
+    books_carrousel[k].classList.add("hidden");
+  }
+}
+
+//Event listener pour affichage des informations d'un livre
+for (let i = 0; i < more.length; i++) {
+  more[i].addEventListener("click", show_more);
+}
+
+//Event listener pour fermeture des informations d'un livre
+for (let i = 0; i < close.length; i++) {
+  close[i].addEventListener("click", close_book);
+}
