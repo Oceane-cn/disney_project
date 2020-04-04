99801edade6852e1f7102f57294669a98376bdb1
Oceane.cn <cn.oceane@gmail.com>
2020-04-04 09:44:34 +0200

    [JS]add scrollReveal library and fix background animation problems

---
+++ src/assets/js/scrollReveal.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal(".books__title", { distance: "50px" });
+ScrollReveal().reveal(".books__container", { distance: "50px", delay: 500 });
+ScrollReveal().reveal(".description", { distance: "50px", delay: 500 });
+ScrollReveal().reveal(".pictures", { distance: "50px", delay: 800 });
